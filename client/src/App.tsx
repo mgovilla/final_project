0061187a3d01c77c7d56fcee04c65a1a30c9f2f5
mgovilla <mgovilla@gmail.com>
2021-10-04 15:03:22 -0400

    testing the editor with json data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'remirror/styles/all.css';
+
+import React, { useCallback } from 'react';
+import { BoldExtension, ItalicExtension } from 'remirror/extensions';
+import { Remirror, useHelpers, useKeymap, useRemirror } from '@remirror/react';
+
+// Hooks can be added to the context without the need for creating custom components
+const hooks = [
+  () => {
+    const { getJSON } = useHelpers();
+
+    const handleSaveShortcut = useCallback(
+      (props) => {
+        const { state } = props;
+        console.log(getJSON(state));
+
+        return true; // Prevents any further key handlers from being run.
+      },
+      [getJSON],
+    );
+
+    // "Mod" means platform agnostic modifier key - i.e. Ctrl on Windows, or Cmd on MacOS
+    useKeymap('Mod-s', handleSaveShortcut);
+  },
+];
 
 function App() {
-  // change when going to production
-  let uri = encodeURI(`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID_LOCAL}&redirect_uri=${process.env.REACT_APP_GITHUB_CALLBACK}`)
-  
+  const { manager, state } = useRemirror({
+    extensions: () => [new BoldExtension(), new ItalicExtension()],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href={uri}
-          rel="noreferrer"
-        >
-          Basic Login
-        </a>
-      </header>
+      <div className="remirror-theme">
+        <Remirror manager={manager} initialContent={state} hooks={hooks}>
+
+        </Remirror>
+      </div>
     </div>
   );
 }
-
 export default App;
