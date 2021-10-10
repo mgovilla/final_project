2b61949e8e62bbe139dd128e99e6a3de4710dc28
mgovilla <mgovilla@gmail.com>
2021-10-10 17:14:26 -0400

    abstract logic to separate home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
-import Modal from 'react-modal';
 import './App.css';
-import useSWR from 'swr'
-import { fetcher, EndPoint } from './util/endpoint'
-import { ResumeForm } from './components/resumeForm';
 
 function App() {
   // change when going to production
   let uri = encodeURI(`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID_LOCAL}&redirect_uri=${process.env.REACT_APP_GITHUB_CALLBACK}`)
 
-  const { data: resumes, error, mutate } = useSWR('/resumes', fetcher('GET'))
-  const [open, setOpen] = useState(false)
-
-  var handleAdd = useCallback(async (s: string) => {
-    console.log(`create new resume: ${s}`)
-    await EndPoint.newResume({title: s});
-    mutate()
-    // redirect to the remix page
-  }, [mutate])
-
-  var handleDelete: React.MouseEventHandler<HTMLButtonElement> = useCallback(async (e) => {
-    console.log('delete new resume')
-    if (resumes.length > 0) {
-      await EndPoint.deleteResume(resumes[0]._id);
-    }
-    mutate()
-    console.log(resumes)
-  }, [mutate, resumes])
-
-
-  Modal.setAppElement('#root');
-
-  if (!error && !resumes) return <p>Loading</p>
-  else return (
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -47,17 +19,6 @@ function App() {
         >
           Basic Login
         </a>
-        {!error && (
-          <div>
-            <button onClick={() => setOpen(true)}>"add resume"</button>
-            <Modal isOpen={open} contentLabel="Minimal Modal Example">
-              <button onClick={() => setOpen(false)}>Close Modal</button>
-              <ResumeForm handleSubmit={handleAdd}></ResumeForm>
-            </Modal>
-            <button onClick={handleDelete}>"delete resume"</button>
-          </div>
-        )}
-        {(!error && resumes) && <ul>{resumes.map((r: any) => <li key={r._id}>{r.title}</li>)}</ul>}
       </header>
     </div>
   );
