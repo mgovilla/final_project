dcc94a4a55e3eb10858fb3b73d883f163391c99c
Shannen Lin <81324780+sklin330@users.noreply.github.com>
2021-10-10 18:19:35 -0400

    work on export functionality

--- client/src/pages/Remix.tsx
+++ client/src/pages/Remix.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Remix.css';
 import MenuLinks from "../components/Sidebar";
+import { jsPDF } from "jspdf";
 
 function Remix () {
+
+  async function exportPDF() {
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const div = window.document.body
+    await doc.html(div);
+    doc.save('test.pdf'); // save / download
+    doc.output('dataurlnewwindow'); // just open it
+  }
+  
   return (
-    <MenuLinks menuStatus="filler"/>
+    <div>
+      <MenuLinks menuStatus="filler"/>
+      <button onClick={ e => exportPDF()}>Export</button>
+    </div>
   )
 }
 
