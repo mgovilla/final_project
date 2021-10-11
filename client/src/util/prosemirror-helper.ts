63ccfd10a14bf39127e7c2383475e56987a66b0d
mgovilla <mgovilla@gmail.com>
2021-10-10 23:22:33 -0400

    fix import

--- client/src/util/prosemirror-helper.ts
+++ client/src/util/prosemirror-helper.ts
@@ -1,14 +1,7 @@
-const {EditorState} = require("prosemirror-state")
 import {Schema, DOMParser, DOMSerializer} from "prosemirror-model";
+const {EditorState} = require("prosemirror-state")
 const {addListNodes} = require("prosemirror-schema-list")
 const {schema} = require("prosemirror-schema-basic")
-
-//I don't know if this is actually needed
-//
-// const mySchema = new Schema({
-//     nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-//     marks: schema.spec.marks
-//   })
   
 const combineContents = (contents: Array<typeof EditorState>) => {
     const div = document.createElement('div')
