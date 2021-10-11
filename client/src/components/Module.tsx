9a21f15d19f8912cf12d852e1e744bfbb66feacc
KyleWynnM <mcfatterkyle@gmail.com>
2021-10-11 04:50:40 -0400

    working on delete module function

--- client/src/components/Module.tsx
+++ client/src/components/Module.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { mutate } from 'swr';
 import { ResumeContext } from '../pages/Remix';
+import { EndPoint } from '../util/endpoint';
 
 interface Props {
   module: models.Module
@@ -14,10 +16,14 @@ class Module extends React.Component<Props, States> {
     this.state = {
     }
     this.handleToggle = this.handleToggle.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
   // static contextType = ResumeContext
   // declare context: React.ContextType<typeof ResumeContext>
-
+  handleDelete: React.MouseEventHandler<HTMLButtonElement> = (async () => {
+    console.log('delete module: ' + module.id);
+    await EndPoint.deleteModule(module.id);
+  });
   // Toggle the module to be in the resume or not
   handleToggle() {
   }
